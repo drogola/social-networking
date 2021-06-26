09d37750637fcd0feff294b3a6c7df85814a6b63
drogola <gabriel.silva@alfaconcursos.com.br>
2021-06-26 10:44:13 -0300

    user private area

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     root to: "home#index"
     resources :admins
   end
-  root to: "admin/home#index"
+  root to: "user/timeline#index"
+
+  namespace :user do
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
